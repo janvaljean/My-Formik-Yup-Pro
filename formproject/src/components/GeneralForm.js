0ec213677fa9996c8d0c2ba6e25891422c1f19b5
janvaljean <kurtuldu@gmx.de>
2023-04-04 22:27:50 +0200

    generallform done

--- formproject/src/components/GeneralForm.js
+++ formproject/src/components/GeneralForm.js
@@ -1,19 +1,34 @@
 import React from 'react'
  import { useFormik } from 'formik';
+import { basicShema } from '../schemas';
+
+
+const onSubmit = async(values, actions) => {
+
+  await new Promise((resolve) => {
+    setTimeout(resolve,1000)
+    //! after one second can be submitted
+  })
+console.log(values)
+  actions.resetForm()
+
+};
 
 const GeneralForm = () => {
-    const {values,errors,handleChange,handleSubmit} = useFormik({
+    const {values, errors, handleChange, handleSubmit, isSubmitting } = useFormik({
      initialValues: {
        email: '',
        age:"",
        password: '',
        confirmPassword: '',
      },
-    
+    validationSchema: basicShema,
+    onSubmit,
+
    });
    
   return (
-  <form>
+  <form onSubmit={handleSubmit}>
     <div className="inputDiv">
         <label htmlFor="">Email</label>
         <input type="email"
@@ -21,7 +36,12 @@ const GeneralForm = () => {
         id='email'
         onChange={handleChange}
         placeholder='Enter a valid email address'
+        className={errors.email ? "input-error" : ""}
+        // error={touched.email && Boolean(errors.email)}
+        // onBlur={handleBlur}
+        // helperText={touched.email && errors.email}
          />
+        {errors.email && <p className='error'>{errors.email}</p>}
     </div>
     <div className="inputDiv">
         <label htmlFor="">Age</label>
@@ -31,6 +51,7 @@ const GeneralForm = () => {
         onChange={handleChange}
         placeholder='Age of the user'
          />
+         {errors.age && <p className='error'>{errors.age}</p>}
     </div>
     <div className="inputDiv">
         <label htmlFor="">Password</label>
@@ -40,6 +61,7 @@ const GeneralForm = () => {
         onChange={handleChange}
         placeholder='Enter a valid password address'
          />
+         {errors.password && <p className='error'>{errors.password}</p>}
     </div>
         <div className="inputDiv">
         <label htmlFor="">Confirm Password</label>
@@ -49,8 +71,9 @@ const GeneralForm = () => {
         onChange={handleChange}
         placeholder='confirm your password'
          />
+          {errors.confirmPassword && <p className='error'>{errors.confirmPassword}</p>}
     </div>
-    <button type='submit'>Submit</button>
+    <button disabled={isSubmitting} type='submit'>Submit</button>
 
   </form>
     
--- formproject/src/components/PortalForm.js
+++ formproject/src/components/PortalForm.js
@@ -1,10 +1,42 @@
+import { Field, Form, Formik } from 'formik';
 import React from 'react'
 
 const PortalForm = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+       <Formik
+       initialValues={{ email: '', color: 'red', firstName: '', lastName: '' }}
+       onSubmit
+     >
+       {(props: FormikProps<any>) => (
+         <Form>
+           <Field type="email" name="email" placeholder="Email" />
+           <Field as="select" name="color">
+             <option value="red">Red</option>
+             <option value="green">Green</option>
+             <option value="blue">Blue</option>
+           </Field>
+ 
+           <Field name="lastName">
+             {({
+               field, // { name, value, onChange, onBlur }
+               form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+               meta,
+             }) => (
+               <div>
+                 <input type="text" placeholder="Email" {...field} />
+                 {meta.touched && meta.error && (
+                   <div className="error">{meta.error}</div>
+                 )}
+               </div>
+             )}
+           </Field>
+           <Field name="lastName" placeholder="Doe" component={MyInput} />
+           <button type="submit">Submit</button>
+         </Form>
+       )}
+     </Formik>
+    </>
   )
 }
 
