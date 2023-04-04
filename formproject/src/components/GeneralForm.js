79c16ffd1e8dc522f339f324f9cb8726227d7051
janvaljean <kurtuldu@gmx.de>
2023-04-04 21:29:07 +0200

    validation shema done

--- formproject/src/components/GeneralForm.js
+++ formproject/src/components/GeneralForm.js
@@ -8,7 +8,6 @@ const GeneralForm = () => {
        age:"",
        password: '',
        confirmPassword: '',
-
      },
     
    });
--- formproject/src/schemas/index.js
+++ formproject/src/schemas/index.js
@@ -1,3 +1,25 @@
 import * as yup from "yup";
+const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
 
-ex
+export const basicShema = yup.object().shape({
+    email: yup
+        .string()
+        .email("Please enter  valid email address")
+        .required("please enter your email address"),
+    age: yup
+        .number()
+        .positive()
+        .integer()
+        .required(),
+    password: yup
+        .string()
+        .min(5,"5 character required")
+        .matches(passwordRules,{
+            message:"required enter a small character and big character and a number "
+        })
+        .required(),
+    confirmPassword: yup
+        .string()
+        .oneOf([yup.ref("password")], "password does not match")
+        .required(),
+})
