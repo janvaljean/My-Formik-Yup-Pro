0a40996defa4f7721bf034cf25b799d79d545c1c
janvaljean <kurtuldu@gmx.de>
2023-04-05 01:38:25 +0200

    portal Form done

--- formproject/src/components/GeneralForm.js
+++ formproject/src/components/GeneralForm.js
@@ -1,83 +1,90 @@
-import React from 'react'
- import { useFormik } from 'formik';
-import { basicShema } from '../schemas';
+import React from "react";
+import { useFormik } from "formik";
+import { basicSchema } from "../schemas";
+import { Link } from "react-router-dom";
 
-
-const onSubmit = async(values, actions) => {
-
+const onSubmit = async (values, actions) => {
+  
   await new Promise((resolve) => {
-    setTimeout(resolve,1000)
-    //! after one second can be submitted
-  })
-console.log(values)
-  actions.resetForm()
-
+    setTimeout(resolve, 1000);
+  });
+  actions.resetForm();
 };
 
-const GeneralForm = () => {
-    const {values, errors, handleChange, handleSubmit, isSubmitting } = useFormik({
-     initialValues: {
-       email: '',
-       age:"",
-       password: '',
-       confirmPassword: '',
-     },
-    validationSchema: basicShema,
-    onSubmit,
+function GeneralForm() {
+  const { values, errors, isSubmitting, handleChange, handleSubmit } =
+    useFormik({
+      initialValues: {
+        email: "",
+        age: "",
+        password: "",
+        confirmPassword: "",
+      },
+      validationSchema: basicSchema,
+      onSubmit,
+    });
 
-   });
-   
+  //   console.log(formik);
   return (
-  <form onSubmit={handleSubmit}>
-    <div className="inputDiv">
-        <label htmlFor="">Email</label>
-        <input type="email"
-        value={values.email}
-        id='email'
-        onChange={handleChange}
-        placeholder='Enter a valid email address'
-        className={errors.email ? "input-error" : ""}
-        // error={touched.email && Boolean(errors.email)}
-        // onBlur={handleBlur}
-        // helperText={touched.email && errors.email}
-         />
-        {errors.email && <p className='error'>{errors.email}</p>}
-    </div>
-    <div className="inputDiv">
-        <label htmlFor="">Age</label>
-        <input type="age"
-        value={values.age}
-        id='age'
-        onChange={handleChange}
-        placeholder='Age of the user'
-         />
-         {errors.age && <p className='error'>{errors.age}</p>}
-    </div>
-    <div className="inputDiv">
-        <label htmlFor="">Password</label>
-        <input type="password"
-        value={values.password}
-        id='password'
-        onChange={handleChange}
-        placeholder='Enter a valid password address'
-         />
-         {errors.password && <p className='error'>{errors.password}</p>}
-    </div>
-        <div className="inputDiv">
-        <label htmlFor="">Confirm Password</label>
-        <input type="password"
-        value={values.confirmPassword}
-        id='confirmPassword'
-        onChange={handleChange}
-        placeholder='confirm your password'
-         />
-          {errors.confirmPassword && <p className='error'>{errors.confirmPassword}</p>}
-    </div>
-    <button disabled={isSubmitting} type='submit'>Submit</button>
-
-  </form>
-    
-  )
+    <form onSubmit={handleSubmit}>
+      <div className="inputDiv">
+        <label>Email</label>
+        <input
+          type="email"
+          value={values.email}
+          onChange={handleChange}
+          id="email"
+          placeholder="Mail adresinizi giriniz"
+          className={errors.email ? "input-error" : ""}
+        />
+        {errors.email && <p className="error">{errors.email}</p>}
+      </div>
+      <div className="inputDiv">
+        <label>Yaş</label>
+        <input
+          type="number"
+          value={values.age}
+          onChange={handleChange}
+          id="age"
+          placeholder="Yaşınızı giriniz"
+          className={errors.age ? "input-error" : ""}
+        />
+        {errors.age && <p className="error">{errors.age}</p>}
+      </div>
+      <div className="inputDiv">
+        <label>Şifre</label>
+        <input
+          type="password"
+          value={values.password}
+          onChange={handleChange}
+          id="password"
+          placeholder="Şifrenizi giriniz"
+          className={errors.password ? "input-error" : ""}
+        />
+        {errors.password && <p className="error">{errors.password}</p>}
+      </div>
+      <div className="inputDiv">
+        <label>Şifre Tekrar</label>
+        <input
+          type="password"
+          value={values.confirmPassword}
+          onChange={handleChange}
+          id="confirmPassword"
+          placeholder="Şifrenizi tekrar giriniz"
+          className={errors.confirmPassword ? "input-error" : ""}
+        />
+        {errors.confirmPassword && (
+          <p className="error">{errors.confirmPassword}</p>
+        )}
+      </div>
+      <button disabled={isSubmitting} type="submit">
+        Kaydet
+      </button>
+      <Link className="formLink" to="/portal">
+        Portala Git
+      </Link>
+    </form>
+  );
 }
 
-export default GeneralForm
+export default GeneralForm;
--- formproject/src/components/PortalForm.js
+++ formproject/src/components/PortalForm.js
@@ -1,43 +1,57 @@
-import { Field, Form, Formik } from 'formik';
-import React from 'react'
+import React from "react";
+import { Form, Formik } from "formik";
+import CustomInput from "./CustomInput";
+import { advancedSchema } from "../schemas";
+import CustomSelect from "./CustomSelect";
+import CustomCheckbox from "./CustomCheckbox";
+import { Link } from "react-router-dom";
 
-const PortalForm = () => {
+const onSubmit = async (values, actions) => {
+  await new Promise((resolve) => {
+    setTimeout(resolve, 1000);
+  });
+  actions.resetForm();
+};
+
+function PortalForm() {
   return (
     <>
-       <Formik
-       initialValues={{ email: '', color: 'red', firstName: '', lastName: '' }}
-       onSubmit
-     >
-       {(props: FormikProps<any>) => (
-         <Form>
-           <Field type="email" name="email" placeholder="Email" />
-           <Field as="select" name="color">
-             <option value="red">Red</option>
-             <option value="green">Green</option>
-             <option value="blue">Blue</option>
-           </Field>
- 
-           <Field name="lastName">
-             {({
-               field, // { name, value, onChange, onBlur }
-               form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
-               meta,
-             }) => (
-               <div>
-                 <input type="text" placeholder="Email" {...field} />
-                 {meta.touched && meta.error && (
-                   <div className="error">{meta.error}</div>
-                 )}
-               </div>
-             )}
-           </Field>
-           <Field name="lastName" placeholder="Doe" component={MyInput} />
-           <button type="submit">Submit</button>
-         </Form>
-       )}
-     </Formik>
+      <Formik
+        initialValues={{ username: "", university: "", isAccepted: false }}
+        onSubmit={onSubmit}
+        validationSchema={advancedSchema}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <CustomInput
+              label="Kullancı Adı"
+              name="username"
+              type="text"
+              placeholder="Kullanıcı Adınızı Giriniz"
+            />
+            <CustomSelect
+              label="Okulunuz"
+              name="university"
+              placeholder="Kullanıcı Üniversitenizi Seçiniz"
+            >
+              <option value="">Lütfen Üniversiteniz Seçiniz</option>
+              <option value="bogazici">Boğaziçi Üniversitesi</option>
+              <option value="gsu">Galatasaray Üniversitesi</option>
+              <option value="odtü">ODTÜ</option>
+              <option value="itü">İTÜ</option>
+            </CustomSelect>
+            <CustomCheckbox type="checkbox" name="isAccepted" />
+            <button disabled={isSubmitting} type="submit">
+              Kaydet
+            </button>
+            <Link className="formLink" to="/">
+              Ana Forma Git
+            </Link>
+          </Form>
+        )}
+      </Formik>
     </>
-  )
+  );
 }
 
-export default PortalForm
+export default PortalForm;
